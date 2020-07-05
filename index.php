f0c9319c3fc5a10537f0c170f806405d1dcf520e
Mihail Petrov <mail@mihail-petrov.me>
2020-07-05 20:27:19 +0300

    Create basic index.php template for displaying blog posts. Style according to the development specification.

---
+++ index.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+        <title>title</title>
+        <link rel="stylesheet" href="css/style.css"/>
+    </head>
+    <body>
+      
+        <div id="header">
+            <h1>@CMS</h1>
+        </div>
+        <div id="content">
+            
+            <ul id="category-placeholder">
+                <li class="category-placeholder--category">Клюки</li>
+                <li class="category-placeholder--category">Политика</li>
+                <li class="category-placeholder--category">Програмиране</li>
+            </ul>
+            
+            <div id="blog-post--content">
+                
+                <div class="post">
+                    <span class="post-title"> Последни клюки</span>
+                    <div class="post-content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore </p>                        
+                    </div>
+                </div>
+
+                <div class="post">
+                    <span class="post-title">Политика служба тика</span>
+                    <div class="post-content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore </p>                        
+                    </div>
+                </div>
+                
+                <div class="post">
+                    <span class="post-title">Сашо Роман президент</span>
+                    <div class="post-content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore </p>                        
+                    </div>
+                </div>
+
+                <div class="post">
+                    <span class="post-title">Излезе HTML 8</span>
+                    <div class="post-content">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore </p>                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id="footer"></div>
+    </body>
+</html>
